cb308e7f30cae8e19286a03480221fb51d2fcdb2
Unknown <camcaswell@gmail.com>
2019-08-07 16:58:24 -0400

    Revert "small tweaks"
    This reverts commit feb8aa2c2fbe8c4533966e701d76367e17638447.

--- items.py
+++
@@ -1,3 +0,0 @@
-class Item():
-    def init():
-        self.recipe = recipe
--- test.py
+++
@@ -1,7 +0,0 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-
-g = nx.Graph()
-g.add_node(3)
-nx.draw(g)
-plt.show()
